864a37164a965f744dd1cd8bd96877c93f0557c7
Guillaume Lajarige <guillaume.lajarige@combodo.com>
2018-07-18 07:46:00 +0000

    itop-iframe-dashlet: Update spanish translations (Thanks to Miguel Turrubiates)
    SVN:trunk[6916]

---
+++ es_cr.dict.itop-iframe-dashlet.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2018 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
+	// Dictionary entries go here
+	'UI:DashletIframe:Label' => 'IFrame',
+	'UI:DashletIframe:Prop-Url' => 'Hipervínculo',
+	'UI:DashletIframe:Prop-Width' => 'Ancho (px)',
+	'UI:DashletIframe:Prop-Height' => 'Alto (px)',
+));
