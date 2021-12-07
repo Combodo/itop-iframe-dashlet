a0fe93ac0f72b98cea5a7c49fbc98f2bb45261aa
acognet <anne-catherine.cognet@combodo.com>
2021-12-07 16:18:27 +0100

    N°3637 - Update spanish translations thanks to Miguel Turrubiates

--- es_cr.dict.itop-iframe-dashlet.php
+++ es_cr.dict.itop-iframe-dashlet.php
@@ -1,11 +1,14 @@
 <?php
-/**
- * Localized data
+ /**
+ * Spanish Localized data
  *
- * @copyright   Copyright (C) 2018 Combodo SARL
+ * @copyright   Copyright (C) 2010-2021 Combodo SARL
  * @license     http://opensource.org/licenses/AGPL-3.0
+ * @traductor   Miguel Turrubiates <miguel_tf@yahoo.com> 
+ * @notas       Utilizar codificación UTF-8 para mostrar acentos y otros caracteres especiales 
  */
-Dict::Add('ES CR', 'Spanish', 'Español, Castellaño', array(
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
 	// Dictionary entries go here
 	'UI:DashletIframe:Label' => 'IFrame',
 	'UI:DashletIframe:Prop-Url' => 'Hipervínculo',
